2114b2faa96d85c133950bfa333da25b3fc5955a
Heber <heberlazo@gmail.com>
2021-06-23 00:42:35 -0500

    Adding game play

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,7 +1,10 @@
-import "phaser";
+import Phaser from "phaser";
+
 export default {
   type: Phaser.AUTO,
-  parent: "phaser-example",
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+  },
 };
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,15 +1,154 @@
-import "phaser";
+import Phaser from "phaser";
+import config from "../Config/config";
+
+// const gameOptions = {
+//   player: "",
+//   stars: "",
+//   platforms: "",
+//   cursors: "",
+//   score: 0,
+//   scoreText: "",
+//   gameOver: false,
+//   bombs: "",
+// };
+
+var player;
+var stars;
+var platforms;
+var cursors;
+var score = 0;
+var scoreText;
+var gameOver = false;
+var bombs;
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("Game");
   }
 
   preload() {
-    // this.load.image("logo", "/src/assets/logo.png");
-    this.load.image("logo", "./assets/zenva_logo.png");
+    this.load.image("sky", "./assets/sky.png");
+    this.load.image("ground", "./assets/platform.png");
+    this.load.image("star", "./assets/star.png");
+    this.load.image("bomb", "./assets/bomb.png");
+    this.load.spritesheet("dude", "./assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
   }
 
   create() {
-    this.add.image(400, 300, "logo");
+    this.add.image(400, 300, "sky");
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 220, "ground");
+
+    player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+      key: "star",
+      repeat: 3,
+      setXY: { x: 80, y: 100, stepX: 200 },
+    });
+
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    bombs = this.physics.add.group();
+
+    scoreText = this.add.text(10, 10, "score: 0", {
+      fontSize: "32px",
+      fill: "#000",
+    });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
   }
+
+  update() {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+
+      player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+
+      player.anims.play("right", true);
+    } else if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-330);
+    } else {
+      player.setVelocityX(0);
+
+      player.anims.play("turn");
+    }
+  }
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
+  score += 10;
+  scoreText.setText("Score: " + score);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    var x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    var bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play("turn");
+
+  gameOver = true;
 }
