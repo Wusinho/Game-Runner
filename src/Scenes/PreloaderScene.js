1f5a747ce8cd370ea3d8d79ea9b0daab4a2bbbf6
Heber <heberlazo@gmail.com>
2021-06-29 18:36:34 -0500

    Remove unnecesary files

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -75,7 +75,7 @@ export default class PreloaderScene extends Phaser.Scene {
       },
     );
 
-    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+    this.timedEvent = this.time.delayedCall(8000, this.ready, [], this);
     this.load.image('blueButton1', "../../assets/ui/blue_button02.png");
     this.load.image('blueButton2', "../../assets/ui/blue_button03.png");
     this.load.image('phaserLogo', "../../assets/logo.png");
